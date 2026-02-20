cbbf04cc0989414e31ba0f02af59a9776094a239
Daniel Taillefer <danieltaillefernieto@gmail.com>
2026-02-20 19:38:34 +0100

    Feuture: Actividad 5 casi lista a falta de rematar detalles

--- src/app/components/blog/blog.components.ts
+++ src/app/components/blog/blog.components.ts
@@ -1,18 +1,32 @@
 import { Component } from '@angular/core';
 import { INoticia } from '../interfaces/inoticia.interfaces';
 import { NOTICIAS } from '../db/noticias.db';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-blog',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './blog.components.html',
   styleUrl: './blog.components.css',
 })
 export class BlogComponents {
+  noticia: INoticia = {titulo: '', urlImagen: '', contenido: '', fecha: ''};
   arrayNoticias: INoticia[] = [];
 
   constructor(){
     this.arrayNoticias = NOTICIAS;
+  }
+
+  guardarNoticia(){
+
+    if(this.noticia.titulo != '' && this.noticia.urlImagen != '' && this.noticia.contenido != '' && this.noticia.fecha != ''){
+      this.arrayNoticias.push({...this.noticia})
+      console.log(this.arrayNoticias)
+      this.noticia = {titulo: '', urlImagen: '', contenido: '', fecha: ''}
+    }else{
+      alert("Todos los campos deben estar rellenos ")
+    }
+
   }
 
 
